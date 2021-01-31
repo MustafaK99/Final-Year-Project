a9e868b1617577b0cdfd0fc9e56a7fa82f96b6ae
MustafaK99 <rmk799@outlook.com>
2021-01-31 18:19:09 +0000

    Graph generation
    Graphs can be generated for region and nation, these contain basic information about number of deaths and cases for the current day

--- Social Distancing App/COVID/test2.py
+++ Social Distancing App/COVID/test2.py
@@ -4,12 +4,10 @@ from datetime import datetime
 import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-ENDPOINT = "https://api.coronavirus.data.gov.uk/v1/data"
-AREA_TYPE = "nation"
-DATE = (datetime.datetime.today().strftime('%Y-%m-%d'))
-AREA_NAME = "england"
+
 
 
 #now = datetime.now()
@@ -17,22 +15,21 @@ AREA_NAME = "england"
 
 
 
-def latestByRegion(ENDPOINT, AREA_TYPE, DATE):
+def getLatestByRegion(ENDPOINT, AREA_TYPE, DATE):
 
 
         filters = [
             f"areaType={ AREA_TYPE }",
             f"date={ DATE }",
-            f"areaName={ AREA_NAME }"
+            
         ]
 
         structure = {
             "areaName":"areaName",
             "date": "date",
             "newCases": "newCasesByPublishDate",
-            "cumulative": "cumCasesByPublishDate",
             "newDeaths": "newDeaths28DaysByPublishDate",
-            "newTestsByPublishDate": "newTestsByPublishDate"
+            "cumDeaths" : "cumDeaths28DaysByPublishDateRate"
      
             
         }
@@ -48,19 +45,124 @@ def latestByRegion(ENDPOINT, AREA_TYPE, DATE):
         if response.status_code >= 400:
             raise RuntimeError(f'Request failed: { response.text }')
 
-        print(response.url)
+        #print(response.url)
         #print(response.json())
         rawJ = response.json()
         df = pd.DataFrame(rawJ["data"])
+       # print(df)
+        return(df)
+
+def getLatestByNation(ENDPOINT, AREA_TYPE, DATE):
+        filters = [
+            f"areaType={ AREA_TYPE }",
+            f"date={ DATE }",
+            
+        ]
+
+        structure = {
+
+            "areaName":"areaName",
+            "date": "date",
+            "newCases": "newCasesByPublishDate",
+            "cumulative": "cumCasesByPublishDate",
+            "newDeaths": "newDeaths28DaysByPublishDate",
+            "cumDeaths" : "cumDeaths28DaysByPublishDateRate"
+     
+            
+        }
+
+        api_params = {
+            "filters": str.join(";", filters),
+            "structure": dumps(structure, separators=(",", ":"))
+        }
+
+
+        response = get(ENDPOINT, params=api_params, timeout=10)
+
+        if response.status_code >= 400:
+            raise RuntimeError(f'Request failed: { response.text }')
+
+       # print(response.url)
+        #print(response.json())
+        rawJ = response.json()
+        df = pd.DataFrame(rawJ["data"])
+       # print(df)
+        return df
+
+        
+
+        
+def latestGraphByRegion():
+        ENDPOINT = "https://api.coronavirus.data.gov.uk/v1/data"
+        AREA_TYPE = "region"
+        DATE = (datetime.datetime.today().strftime('%Y-%m-%d'))
+        df = getLatestByRegion(ENDPOINT, AREA_TYPE, DATE)
         print(df)
 
+       
+        sns.barplot(data=df, x="areaName", y="newCases");
+        plt.xlabel('')
+        plt.ylabel('New cases')
+        plt
+        plt.title('Number of new cases recorded on {} in England by Region'.format(DATE))
+        plt.xticks(size=9)
+        sns.despine();
+        plt.show()
 
+
+
+        
+
+        sns.barplot(data=df, x="areaName", y="newDeaths");
+        plt.xlabel('')
+        plt.ylabel('New Deaths')
+        plt
+        plt.title('Number of new deaths recorded on {} in England by Region'.format(DATE))
+        plt.xticks(size=9)
+        sns.despine();
+        plt.show()
 
 
 
         
 
         
-  
 
-latestByRegion(ENDPOINT, AREA_TYPE, DATE)
+def latestGraphByNation():
+        ENDPOINT = "https://api.coronavirus.data.gov.uk/v1/data"
+        AREA_TYPE = "nation"
+        DATE = (datetime.datetime.today().strftime('%Y-%m-%d'))
+        df = getLatestByNation(ENDPOINT, AREA_TYPE,DATE)
+
+        sns.barplot(data=df, x="areaName", y="newCases");
+        plt.xlabel('')
+        plt.ylabel('New cases')
+        plt
+        plt.title('Number of new cases recorded on {} in the UK by nation'.format(DATE))
+        plt.xticks(size=9)
+        sns.despine();
+        plt.show()
+
+
+
+        
+
+        sns.barplot(data=df, x="areaName", y="newDeaths");
+        plt.xlabel('')
+        plt.ylabel('New Deaths')
+        plt
+        plt.title('Number of new deaths recorded on {} in the UK by nation'.format(DATE))
+        plt.xticks(size=9)
+        sns.despine();
+        plt.show()
+
+
+
+
+        
+
+latestGraphByRegion()
+latestGraphByNation()
+
+
+
