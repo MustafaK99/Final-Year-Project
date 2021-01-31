7896301bdac167c7ecd32eeb353792400292e033
MustafaK99 <rmk799@outlook.com>
2021-01-31 20:32:24 +0000

    Added web routes
    updated flask application for basic web routes

--- Social Distancing App/FlaskApp/app.py
+++ Social Distancing App/FlaskApp/app.py
@@ -3,10 +3,30 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/home')
+@app.route('/index')
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def welcome():
+    return 'welcome'
+
+
+@app.route('/about')
+def about():
+    return 'About the Covid-19 analysis platform'
+
+
+@app.route('/country')
+@app.route('/nation')
+def nation():
+    return 'Covid-19 data by nation'
+
+
+@app.route('/county')
+@app.route('/region')
+def region():
+    return 'Covid 19 data by region/county'
 
 
 if __name__ == '__main__':
+
     app.run()
