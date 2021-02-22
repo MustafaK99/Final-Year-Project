b65c2ca90f4dd54c1bc95235f360c1a5009e4c24
MustafaK99 <rmk799@outlook.com>
2021-02-22 18:21:49 +0000

    Updated routes and added account page
    Added increased validation and more routes

--- Social Distancing App/FlaskApp/app/forms.py
+++ Social Distancing App/FlaskApp/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -11,6 +12,18 @@ class RegistrationForm(FlaskForm):
                                          Length(min=8)])
     confirm = PasswordField('Repeat Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username unavailable, please try again ")
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError("Email unavailable, please try again")
+
+
 
 
 class LoginForm(FlaskForm):
--- Social Distancing App/FlaskApp/app/routes.py
+++ Social Distancing App/FlaskApp/app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, flash, redirect
-from app import app, dataScraper
+from flask import render_template, url_for, flash, redirect, request
+from app import app, dataScraper, db, bcrypt
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Detections
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route('/home')
@@ -31,17 +32,43 @@ def region():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('welcome'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account Created for {form.username.data}', 'Success')
-        return redirect(url_for('welcome'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created! please log in', 'Success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('welcome'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'logged in', 'Success')
-        return redirect(url_for('welcome'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('welcome'))
+        else:
+            flash('Login Unsuccessful, email and/or password are incorrect please try again', 'danger')
     return render_template('login.html', title='login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('welcome'))
+
+
+@app.route("/account")
+@login_required
+def account():
+
+    return render_template('account.html', title='Account')
